33f5e7263265403e98f6ee852188e9ed27006b47
DucThang <139322881+trinhducthang@users.noreply.github.com>
2024-05-24 13:05:21 +0700

    Update UsersController.java

--- ManagerUsers/src/main/java/com/java1504/ManagerUsers/UsersController/UsersController.java
+++ ManagerUsers/src/main/java/com/java1504/ManagerUsers/UsersController/UsersController.java
@@ -1,14 +1,16 @@
 package com.java1504.ManagerUsers.UsersController;
 
 
-import com.java1504.ManagerUsers.InfoBase.Bank;
 import com.java1504.ManagerUsers.UserServices.UserServices;
 import com.java1504.ManagerUsers.InfoBase.Users;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class UsersController {
@@ -16,13 +18,25 @@ public class UsersController {
     private UserServices userServices;
 
     @PostMapping("/add")
-    public Users addUsers(@RequestBody Users users){
-        return userServices.addUsers(users);
+    public ResponseEntity<?> addUsers(@Valid @RequestBody Users users, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<ErrorDetails> errors = bindingResult.getFieldErrors().stream()
+                    .map(error -> new ErrorDetails(error.getField(), error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            Users addedUser = userServices.addUsers(users);
+            return new ResponseEntity<>(addedUser, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 
     @GetMapping
     public List<Users> getAll(){
-
         return userServices.getALL();
     }
 
