7b87f229ba545c926e65c4cbf0507e7a08f37530
thang7904 <trinhducthang.7904@gmail.com>
2024-08-17 16:33:45 +0700

    first commit

--- discoveryserver/src/main/java/com/ducthang/discoveryserver/DiscoveryserverApplication.java
+++ discoveryserver/src/main/java/com/ducthang/discoveryserver/DiscoveryserverApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ducthang.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DemoApplication {
+public class DiscoveryserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DiscoveryserverApplication.class, args);
 	}
 
 }
